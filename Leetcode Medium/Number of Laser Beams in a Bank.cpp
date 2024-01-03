f1b5d858b0d4795ab655280005511637595c7010
Ankita Rawat <115618690+Ankitarawat09@users.noreply.github.com>
2024-01-03 23:10:42 +0530

    Create Number of Laser Beams in a Bank.cpp

---
+++ Leetcode Medium/Number of Laser Beams in a Bank.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+        int n=bank.size();
+        
+        int prevCount=0;
+        int answer=0;
+
+        for(int i=0;i<n;i++){
+            int currCount=0;
+            for(char &ch:bank[i]){
+                if(ch=='1'){
+                    currCount++;
+
+                }
+            }
+            answer+=(currCount*prevCount);
+            if(currCount!=0){
+                prevCount=currCount;
+            }
+        }
+
+        return answer;
+        
+    }
+};
