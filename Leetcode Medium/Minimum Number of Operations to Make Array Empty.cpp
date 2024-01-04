6a22262e32247f6300ea6f2bbf0a9dc8edb6fe7c
Ankita Rawat <115618690+Ankitarawat09@users.noreply.github.com>
2024-01-04 23:58:57 +0530

    Create Minimum Number of Operations to Make Array Empty.cpp

---
+++ Leetcode Medium/Minimum Number of Operations to Make Array Empty.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        unordered_map<int,int>map;
+        for(int &num: nums){
+            map[num]++;
+        }
+
+        int result=0;
+        for(auto &it:map){
+            int count=it.second;
+            if(count==1){
+                return -1;
+            }
+            result+=ceil((double)count/3);
+        }
+        return result;
+        
+    }
+};
